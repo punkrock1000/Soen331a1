09f5266ca38a475ffa9fc9adc1d6984d60917fa3
punkrock1000 <antoineracinegingras@hotmail.com>
2019-02-10 16:11:46 -0500

    added email, fixed typo

--- simulation.pl
+++ simulation.pl
@@ -2,15 +2,14 @@
 %%
 %%  CONCORDIA UNIVERSITY
 %%  Department of Computer Science and Software Engineering
-%%  SOEN 331-W:  Assingment 1
+%%  SOEN 331-W:  Assignment 1
 %%  Winter term, 2019
 %%  Date submitted: February 11, 2019
 %%
-%%  Authors: 
-%%
-%%  name1, id1, email1
-%%  name2, id2, email2 
-%%  ...
+%%  Authors: 	Tran Hai Vong Thang, 40004591, vongthang93@gmail.com
+%%				Justin Weller, 40026870, jadamweller@gmail.com
+%%				Antoine Racine-Gingras, 40008247, antoineracinegingras@hotmail.com
+%%				Vatika Prasad, 40033221, vatikaprasad@gmail.com
 %%
 %% =============================================================================
 
@@ -19,7 +18,6 @@
 %%  Facts
 %%
 %% =============================================================================
-
 
 
 
